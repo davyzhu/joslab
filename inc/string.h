77a93dd9cb6b48d542e860c0bd39aab0727766a5
Zhu, Shenli <zhushenli@gmail.com>
2012-02-29 15:46:56 +0800

    add useful monitor functions

--- inc/string.h
+++ inc/string.h
@@ -12,6 +12,7 @@ int	strcmp(const char *s1, const char *s2);
 int	strncmp(const char *s1, const char *s2, size_t size);
 char *	strchr(const char *s, char c);
 char *	strfind(const char *s, char c);
+uint32_t str2hex(const char *s); 
 
 void *	memset(void *dst, int c, size_t len);
 /* no memcpy - use memmove instead */
